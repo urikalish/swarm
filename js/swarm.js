4299f0ebd96f7f65fdf35698ed16516a8ea9b755
urikalish <uri.kalish@gmail.com>
2019-10-05 04:33:29 +0300

    wip

--- js/swarm.js
+++ js/swarm.js
@@ -2,56 +2,56 @@ const canvasWidth = 1600;
 const canvasHeight = 800;
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const items = [];
-const itemSize = 10;
+const particles = [];
+const particleSize = 4;
 
 const init = () => {
     for (let i = 0; i < 100; i++) {
-        items.push(
+        particles.push(
         {
-            w: itemSize,
-            h: itemSize,
-            x: canvasWidth/2 - itemSize/2,
-            y: canvasHeight/2 - itemSize/2,
+            w: particleSize,
+            h: particleSize,
+            x: Math.trunc(Math.random() * (canvasWidth - particleSize)),
+            y: Math.trunc(Math.random() * (canvasHeight - particleSize)),
             vx: 0,
             vy: 0,
             ax: 0,
             ay: 0,
             c: {
-                    h: Math.trunc(360*Math.random()),
+                    h: Math.trunc(Math.random() * 360),
                     s: 100,
-                    l: 25 + Math.trunc(50*Math.random())
+                    l: 25 + Math.trunc(Math.random() * 50)
                 }            
         });
     }
 };
 
 const clear = () => {
-    items.forEach(item => {
+    particles.forEach(p => {
         ctx.fillStyle = "black";
-        ctx.fillRect(Math.round(item.x), Math.round(item.y), item.w, item.h);
+        ctx.fillRect(Math.round(p.x), Math.round(p.y), p.w, p.h);
     })
 };
 
 const move = () => {
-    items.forEach(item => {
-        item.ax = Math.random() * 1 - 0.5;
-        item.ay = Math.random() * 1 - 0.5;
-        item.vx += item.ax;
-        item.vy += item.ay;
-        item.vx = Math.max(-3, Math.min(3, item.vx));
-        item.vy = Math.max(-3, Math.min(3, item.vy));
-        item.x += item.vx;
-        item.y += item.vy;
-        item.x = Math.max(0, Math.min(canvasWidth-itemSize, item.x));
-        item.y = Math.max(0, Math.min(canvasHeight-itemSize, item.y));
+    particles.forEach(p => {
+        p.ax = Math.random() * 1 - 0.5;
+        p.ay = Math.random() * 1 - 0.5;
+        p.vx += p.ax;
+        p.vy += p.ay;
+        p.vx = Math.max(-3, Math.min(3, p.vx));
+        p.vy = Math.max(-3, Math.min(3, p.vy));
+        p.x += p.vx;
+        p.y += p.vy;
+        p.x = Math.max(0, Math.min(canvasWidth-particleSize, p.x));
+        p.y = Math.max(0, Math.min(canvasHeight-particleSize, p.y));
     })
 };
 
 const draw = () => {
-    items.forEach(item => {
-        ctx.fillStyle = `hsl(${item.c.h}, ${item.c.s}%, ${item.c.l}%)`;
-        ctx.fillRect(Math.round(item.x), Math.round(item.y), item.w, item.h);
+    particles.forEach(p => {
+        ctx.fillStyle = `hsl(${p.c.h}, ${p.c.s}%, ${p.c.l}%)`;
+        ctx.fillRect(Math.round(p.x), Math.round(p.y), p.w, p.h);
     })
 };
 
