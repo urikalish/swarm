cd5ee2c0532694824586277bdf503b1f36268d93
Uri Kalish <urikalish@users.noreply.github.com>
2019-10-16 20:58:18 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ const initGame = () => {
             {
                 id: 1,
                 name: 'Player1',
-                color: Helper.strToHsl('#ff33ff')
+                color: Helper.strToHsl('#cc33cc')
             }
         ]
     );
